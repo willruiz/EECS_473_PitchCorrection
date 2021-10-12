98983cf0686e9a3bf2c4356b19c23b020f2ebbe3
Ellen Reed <56777162+ellreed@users.noreply.github.com>
2021-10-12 12:48:05 -0400

    Update microphone.c

--- Src/microphone.c
+++ Src/microphone.c
@@ -15,7 +15,29 @@ enum Note {A, As, B, C, Cs, D, Ds, E, F, Fs, G, Gs}; // 's' indicates sharp
 Note curr_note;
 uint8_t curr_octave; 
 float perc_error;
-  
+uint32_t recieve_buffer[WORDS_IN_RX_BLOCK]; // how many words to receive at one time??
+nrfx_i2s_buffers_t initial_buffers;
+
+// data_handler  
+static void data_handler(nrfx_i2s_buffers_t const *p_released, u32_t status)
+{
+	if (NRFX_I2S_STATUS_NEXT_BUFFERS_NEEDED == status)
+	{
+		nrfx_err_t err = nrfx_i2s_next_buffers_set(&initial_buffers);
+		if (err != NRFX_SUCCESS)
+		{
+			printk("Error!, continuing running as if nothing happened, but you should probably investigate.\n");
+		}
+	}
+	if (p_released)
+	{
+		if (p_released->p_rx_buffer != NULL)
+		{
+			data_ready_flag = true; 
+		}
+	}
+}
+
 // initialize the pin to be used with the microphone.
 void microphone_init(uint8_t LRCL_pin, uint8_t DOUT_pin, uint8_t BCLK_pin) {
   // initalize i2s connection with these pins
@@ -27,14 +49,34 @@ void microphone_init(uint8_t LRCL_pin, uint8_t DOUT_pin, uint8_t BCLK_pin) {
 
 	config.mode = NRF_I2S_MODE_MASTER;			//Microphone requirement
 	config.ratio = NRF_I2S_RATIO_64X;			//Microphone requirement
-	config.sample_width = NRF_I2S_SWIDTH_24BIT_IN32BIT; //Microphone requirement
+	config.sample_width = NRF_I2S_SWIDTH_24BIT_IN32BIT; 	//Microphone requirement
+	
+	// TODO: possibly change for feather
 	config.mck_setup = NRF_I2S_MCK_32MDIV31;	//Preference     freq = (MCKfreq/ratio) =16.129 KHz.
 	config.channels = NRF_I2S_CHANNELS_LEFT;	//Preference
+	
+	nrfx_err_t err_code = nrfx_i2s_init(&config, data_handler);
+	if (err_code != NRFX_SUCCESS)
+	{
+		printk("I2S init error\n");
+		return err_code;
+	}
 }
 
-// read in the current value of the microphone, use in interrrupts
+// read in the current value of the microphone, call this fxn in interrrupts
 float microphone_read() {
    // initiate i2s recieve 
+	memset(&recieve_buffer, 0x00, sizeof(recieve_buffer));
+	initial_buffers->p_rx_buffer = recieve_buffer;
+	
+	err_code = nrfx_i2s_start(&initial_buffers, I2S_DATA_BLOCK_WORDS, 0); //start recording
+	if (err_code != NRFX_SUCCESS)
+	{
+		printk("I2S start error\n");
+		return err_code;
+	}
+	//nrfx_i2s_stop() //stop recording
+	
 }
   
 // tuning, error checking, note identifying algorithm
