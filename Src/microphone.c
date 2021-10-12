41ebaa5676b7bb59a838a7dac7acc8b808faa045
Ellen Reed <56777162+ellreed@users.noreply.github.com>
2021-10-12 12:11:54 -0400

    add default config

--- Src/microphone.c
+++ Src/microphone.c
@@ -19,7 +19,17 @@ float perc_error;
 // initialize the pin to be used with the microphone.
 void microphone_init(uint8_t LRCL_pin, uint8_t DOUT_pin, uint8_t BCLK_pin) {
   // initalize i2s connection with these pins
-  
+  // TODO: change this config to fit your MEMS microphone and audio processing needs.
+	nrfx_i2s_config_t config =
+		NRFX_I2S_DEFAULT_CONFIG(BCLK_pin, LRCL_pin,
+								NRFX_I2S_PIN_NOT_USED,
+								NRFX_I2S_PIN_NOT_USED, DOUT_pin); // NRFX_I2S_DEFAULT_CONFIG(_pin_sck, _pin_lrck, _pin_mck, _pin_sdout, _pin_sdin)
+
+	config.mode = NRF_I2S_MODE_MASTER;			//Microphone requirement
+	config.ratio = NRF_I2S_RATIO_64X;			//Microphone requirement
+	config.sample_width = NRF_I2S_SWIDTH_24BIT_IN32BIT; //Microphone requirement
+	config.mck_setup = NRF_I2S_MCK_32MDIV31;	//Preference     freq = (MCKfreq/ratio) =16.129 KHz.
+	config.channels = NRF_I2S_CHANNELS_LEFT;	//Preference
 }
 
 // read in the current value of the microphone, use in interrrupts
