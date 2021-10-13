fcbced48a296ab4aef22b76e10411811c7d64218
Quintin Harter <qharter@gmail.com>
2021-10-13 16:10:55 -0400

    nrf Application push

---
+++ build/zephyr/include/generated/ncs_version.h
@@ -0,0 +1,13 @@
+#ifndef _NCS_VERSION_H_
+#define _NCS_VERSION_H_
+
+#define NCS_VERSION_CODE 67328
+#define NCS_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+
+#define NCS_VERSION_NUMBER  0x10700
+#define NCS_VERSION_MAJOR   1
+#define NCS_VERSION_MINOR   7
+#define NCS_PATCHLEVEL      0
+#define NCS_VERSION_STRING  "1.7.0"
+
+#endif /* _NCS_VERSION_H_ */
---
+++ build/zephyr/nrf52_adafruit_feather.dts.pre.d
@@ -0,0 +1,12 @@
+empty_file.o: C:/Users/qhart/ncs/zephyr/misc/empty_file.c \
+ C:/Users/qhart/ncs/zephyr/boards/arm/nrf52_adafruit_feather/nrf52_adafruit_feather.dts \
+ C:/Users/qhart/ncs/zephyr/dts/arm/nordic/nrf52832_qfaa.dtsi \
+ C:/Users/qhart/ncs/zephyr/dts/common/mem.h \
+ C:/Users/qhart/ncs/zephyr/dts/arm/nordic/nrf52832.dtsi \
+ C:/Users/qhart/ncs/zephyr/dts/arm/armv7-m.dtsi \
+ C:/Users/qhart/ncs/zephyr/dts/common/skeleton.dtsi \
+ C:/Users/qhart/ncs/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ C:/Users/qhart/ncs/zephyr/include/dt-bindings/i2c/i2c.h \
+ C:/Users/qhart/ncs/zephyr/include/dt-bindings/gpio/gpio.h \
+ C:/Users/qhart/ncs/zephyr/dts/arm/nordic/override.dtsi \
+ C:/Users/qhart/ncs/zephyr/boards/arm/nrf52_adafruit_feather/feather_connector.dtsi
